66bd94e440f2fb70d784dcf6a08690f6b7bd3a61
mahdi chekini <mchekini@checkconsulting.fr>
2025-03-09 20:31:03 +0100

    init project

---
+++ src/main/java/fr/formationacademy/scpiinvestplusnotification/ScpiInvestPlusNotificationApplication.java
@@ -0,0 +1,13 @@
+package fr.formationacademy.scpiinvestplusnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpiInvestPlusNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScpiInvestPlusNotificationApplication.class, args);
+    }
+
+}
